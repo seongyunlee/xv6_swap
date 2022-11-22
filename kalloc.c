027cf91a63dc51388e9511dcfbdc3ee6bcc60829
seongyunlee <priavate.seongyun@gmail.com>
2022-11-23 01:33:28 +0900

    good night

--- kalloc.c
+++ kalloc.c
@@ -240,5 +240,11 @@ void lru_pop(char* va,pde_t *pgdir,int pa){
     cur=cur->next;
   }
   panic("no pop");
+  for(int i=0;i<num_lru_pages;i++){
+    if(cur==p){
+      cprintf("%x\n",(int)p);
+    }
+    cur=cur->next;
+  }
   release(&lru_head_lock);
 }
