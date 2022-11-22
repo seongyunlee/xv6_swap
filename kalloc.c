df70a6bdaf2e11cdf8f10ab82c8107ccb1dd3aea
seongyunlee <priavate.seongyun@gmail.com>
2022-11-23 01:35:23 +0900

    good night

--- kalloc.c
+++ kalloc.c
@@ -239,6 +239,7 @@ void lru_pop(char* va,pde_t *pgdir,int pa){
     }
     cur=cur->next;
   }
+  cprintf("num lru pages %d\n",num_lru_pages);
   for(int i=0;i<num_lru_pages;i++){
     if(cur==p){
       cprintf("%x\n",(int)p);
