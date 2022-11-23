ca01428d57451ec3d726403c57c66071c502301d
seongyunlee <priavate.seongyun@gmail.com>
2022-11-23 10:09:02 +0900

    good night

--- swaptest.c
+++ swaptest.c
@@ -8,14 +8,19 @@
 #include "traps.h"
 #include "memlayout.h"
 
+char* arr[100];
 
 int main () {
-    
-
     int *k = (int*)malloc(4);
     for (int i =0 ; i<100;i++){
         printf(1,"when swap? %d\n",i);
-        if((int)sbrk(409600)==-1) break;
+        char* p = sbrk(409600);
+        if(p==-1) break;
+        *p = 'c';
+        arr[i]=p;
+    }
+    for(int i=0;i<100;i++){
+        printf("print %d : %x ->%c\n",i,(int)arr[i],*arr[i]);
     }
     printf(1,"read after swapping %x %d",(int)k,*k);
     int a,b;
